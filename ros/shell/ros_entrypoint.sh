8c132c204d5594e4e29d1e7338fc40024e564dfb
waxz <wz67766@163.com>
2018-09-11 16:55:05 +0800

    update Dockerfile

--- ros/shell/ros_entrypoint.sh
+++ ros/shell/ros_entrypoint.sh
@@ -3,12 +3,10 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-echo "run ros_entrypoint ... done"
+echo "run ros_entrypoint ... "
 
 #set environment
 # fix x11 bug
 LC_ALL=C
-
-cd /ros/catkin_ws
-python app.py
+#if [ ! $1 -a $1 == "app" ];then cd /ros/catkin_ws;python app.py;elif [ $1 == "bash" ]; then bash ;fi
 exec "$@"
